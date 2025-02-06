bfe32cc3d9403b21d3cd29750c908dc08eb3c830
Yanni Fraga <yanni_fernanda@hotmail.com>
2025-02-06 11:39:23 +0100

    Apply forces

--- physics/src/script.js
+++ physics/src/script.js
@@ -67,6 +67,7 @@ const sphereBody = new CANNON.Body({
   shape: sphereShape,
   // material: plasticMaterial
 })
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody)
 
 // floor
@@ -188,6 +189,7 @@ const tick = () =>
   oldElapsedTime = elapsedTime
 
   // update physics world
+  sphereBody.applyForce(new CANNON.Vec3(-.5, 0, 0), sphereBody.position)
   // 60 = 60 fps
   world.step(1 / 60, deltaTime, 3)
 
